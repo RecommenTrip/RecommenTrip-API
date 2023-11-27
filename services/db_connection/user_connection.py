1352c7b38bbad33df8175c0897c50d205c8a30fd
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2023-11-27 16:20:03 -0400

    new register creation in places.csv

--- services/db_connection/user_connection.py
+++ services/db_connection/user_connection.py
@@ -11,7 +11,16 @@ class UserConnection:
 
             # Append the new record to the CSV file
             new_record_df.to_csv(self.file_path, mode='a', header=False, index=False)
+
+            places_df = pd.read_csv('datasets/places.csv')
+            print(len(places_df))
+            # Add a row of zeros to the CSV file
+            zeros_df = pd.DataFrame([[0] * len(places_df.columns)])
+            zeros_df.to_csv('datasets/places.csv', mode='a', header=False, index=False)
+
             return True  # Indicates successful addition
+            
+            
         except Exception as e:
             print(f"Error: {e}")
             return False  # Indicates failure
